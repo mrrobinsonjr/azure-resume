0438703c6fb15ed7666b35b2c1b071682518a6ee
Michael Robinson <michaelrobinsonjr@gmail.com>
2022-12-24 17:53:02 -0500

    updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+}
+get
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).them(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.")
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function (erroer) {
+        console.log(error);
+    });
+    return count
+
+}
