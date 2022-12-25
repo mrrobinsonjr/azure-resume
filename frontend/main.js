5080b1efddef58c4a4c046383078e188b250913a
Michael Robinson <michaelrobinsonjr@gmail.com>
2022-12-25 17:16:55 -0500

    Updated main.js

--- frontend/main.js
+++ frontend/main.js
@@ -5,13 +5,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
+
+
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).them(response => {
+    fetch(functionApi).then(response => {
         return response.json()
     }).then(response =>{
-        console.log("Website called function API.")
-        count = response.count;
+        console.log("Website called function API.");
+        count =  response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
