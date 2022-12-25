ee56debfa31fa753853c9ad5fbeea93f0659e5d9
Michael Robinson <michaelrobinsonjr@gmail.com>
2022-12-25 15:30:27 -0500

    Created azure function APA

--- frontend/main.js
+++ frontend/main.js
@@ -3,19 +3,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
 })
 
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).them(response => {
         return response.json()
-    }).then(response => {
+    }).then(response =>{
         console.log("Website called function API.")
         count = response.count;
         document.getElementById("counter").innerText = count;
-    }).catch(function (erroer) {
+    }).catch(function(error){
         console.log(error);
     });
-    return count
+    return count;
 
 }
