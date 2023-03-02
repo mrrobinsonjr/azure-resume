6a948b10d192e261bef02f06d35fa610bd06a09e
michaelrobinsonjr <michaelrobinsonjr@gmail.com>
2023-03-02 18:57:52 -0500

    Update

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -1,22 +1,29 @@
 using Microsoft.Extensions.Logging;
 using Xunit;
+using Moq;
 
 namespace tests
 {
     public class TestCounter
     {
-        private readonly ILogger logger = TestFactory.CreateLogger();
+        private readonly Mock<ILogger> loggerMock = new Mock<ILogger>();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public async Task Http_trigger_should_return_known_string()
         {
-            var counter = new Company.Function.Counter();
-            counter.Id = "1";
-            counter.Count = 2;
-            var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) Company.Function.azure-resume-challenge.Run(request, counter, out counter, logger);
-            Assert.Equal(3, counter.Count);
-        }
+            var counter = new Company.Function.Counter
+            {
+                Id = "1",
+                Count = 2
+            };
 
+            var request = TestFactory.CreateHttpRequest();
+
+            var result = await Company.Function.GetResumeCounter.RunAsync(request, counter, Mock.Of<IAsyncCollector<Company.Function.Counter>>(), loggerMock.Object);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var counterResult = Assert.IsType<Company.Function.Counter>(okResult.Value);
+            Assert.Equal(3, counterResult.Count);
+        }
     }
 }
