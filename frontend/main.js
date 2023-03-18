e5ed582f15a16cf126aec558e943e5a669af35db
Michael <michaelrobinsonjr@Gmail.com>
2023-03-18 15:08:09 -0400

    Updates to structure and created main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,34 @@
+// This function is called when the webpage has finished loading
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApiURL = "https://azure-resume-challenge.azurewebsites.net/api/GetResumeCounter?code=dUGHtOoYjbPSD6DRWviG4EIEFx581mGK_Pp8cojI5xiZAzFurJHZVQ=="
-const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+// URL for the backend API
+const functionApiURL = ""
 
+// URL for a local test endpoint
+const localfunctionApi = '';
 
-
+// This function fetches the visit count from the backend API and displays it on the webpage
 const getVisitCount = () => {
+    // Set a default count
     let count = 30;
-    fetch(functionApiURL).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
+    
+    // Fetch the visit count from the API
+    fetch(functionApiURL) // Makes an HTTP request to the specified URL
+    .then(response => { // Handles the response object
+        return response.json() // Parses the response as JSON
+    })
+    .then(response =>{ // Handles the parsed JSON data
+        console.log("Website called function API."); // Logs a message to the console
+        count =  response.count; // Extracts the count value from the JSON data
+        document.getElementById("counter").innerText = count; // Displays the count value on the webpage
+    })
+    .catch(function(error){ // Handles any errors that occurred during the request
+        console.log(error); // Logs the error message to the console
     });
+    
+    // Return the visit count
     return count;
 
 }
