d2d1f66335dd7cd49f49ef122ed767eada84b8af
Michael <michaelrobinsonjr@Gmail.com>
2023-03-24 08:42:04 -0400

    Updates to function and main.js

--- backend/api/mrrGetResumeCounter.cs
+++ backend/api/mrrGetResumeCounter.cs
@@ -1,44 +1,50 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
 
 namespace Company.Function
 {
-    public static class mrrGetResumeCounter
+    // Azure Function class for getting and incrementing the resume counter
+    public static class MrrGetResumeCounter
     {
+        // Constants for Cosmos DB configuration
         private const string DatabaseName = "azureresume";
         private const string CollectionName = "counter";
-        private const string Id = "1";
+        private const string DocumentId = "1";
         private const string PartitionKey = "1";
 
-        [FunctionName("mrrGetResumeCounter")]
+        // The main function that gets triggered by an HTTP request
+        [FunctionName("MrrGetResumeCounter")]
         public static async Task<IActionResult> RunAsync(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(DatabaseName, CollectionName, ConnectionStringSetting = "AzureResumeConnectionString", Id = Id, PartitionKey = PartitionKey)] counter counter,
-            [CosmosDB(DatabaseName, CollectionName, ConnectionStringSetting = "AzureResumeConnectionString", Id = Id, PartitionKey = PartitionKey)] IAsyncCollector<counter> updatedCounterCollector,
-            ILogger log)
+            // Input binding for the counter document from Cosmos DB
+            [CosmosDB(DatabaseName, CollectionName, ConnectionStringSetting = "AzureResumeConnectionString", Id = DocumentId, PartitionKey = PartitionKey)] Counter counter,
+            // Output binding for updating the counter document in Cosmos DB
+            [CosmosDB(DatabaseName, CollectionName, ConnectionStringSetting = "AzureResumeConnectionString", Id = DocumentId, PartitionKey = PartitionKey)] IAsyncCollector<Counter> updatedCounterCollector,
+            ILogger log) // Logger instance for logging messages
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            counter.count++;
+            // Increment the counter
+            counter.Count++;
 
+            // Update the counter in Cosmos DB
             await updatedCounterCollector.AddAsync(counter);
 
+            // Return the updated counter as an OK response
             return new OkObjectResult(counter);
         }
     }
-    public class counter
-    {
-        public string id { get; set; }
-        public int count { get; set; }
-    }
 
+    // Class representing the counter document in Cosmos DB
+    public class Counter
+    {
+        // ID property for the document, required by Cosmos DB
+        public string id { get; set; }
+        // Count property representing the current visit count
+        public int Count { get; set; }
+    }
 }
--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,33 @@
+// This event listener triggers when the webpage has finished loading
 window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
+    getVisitCount(); // Call the getVisitCount function
 })
 
-const functionApiURL = "https://azure-resume-challenge.azurewebsites.net/api/GetResumeCounter?code=dUGHtOoYjbPSD6DRWviG4EIEFx581mGK_Pp8cojI5xiZAzFurJHZVQ=="
+// URL for the production backend API
+const functionApiURL = "https://azure-resume-challenge.azurewebsites.net/api/GetResumeCounter?code=dUGHtOoYjbPSD6DRWviG4EIEFx581mGK_Pp8cojI5xiZAzFurJHZVQ==";
+
+// URL for the local backend API (for testing)
 const localfunctionApi = 'http://localhost:8080/api/mrrGetResumeCounter';
 
-
-
+// This function fetches the visit count from the backend API and displays it on the webpage
 const getVisitCount = () => {
+    // Set a default count value
     let count = 30;
-    fetch(localfunctionApi).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
+    
+    // Fetch the visit count from the API
+    fetch(localfunctionApi) // Makes an HTTP request to the specified URL
+    .then(response => { // Handles the response object
+        return response.json() // Parses the response as JSON
+    })
+    .then(response => { // Handles the parsed JSON data
+        console.log("Website called function API."); // Logs a message to the console
+        count = response.count; // Extracts the count value from the JSON data
+        document.getElementById("counter").innerText = count; // Displays the count value on the webpage
+    })
+    .catch(error => { // Handles any errors that occurred during the request
+        console.log(error); // Logs the error message to the console
     });
-    return count;
 
+    // Return the visit count
+    return count;
 }
